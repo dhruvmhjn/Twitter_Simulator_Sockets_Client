3bd0b025698dbec2e52c29bd89b4414c26033f08
dhruvmhjn <dhruvmhjn@gmail.com>
2017-12-12 11:54:51 -0500

    Orc celanup

--- lib/orc.ex
+++ lib/orc.ex
@@ -30,21 +30,16 @@ defmodule Orc do
     end
 
     def handle_cast({:begin_activate},{numClients,acts,subPercent,numRegistered,numCompleted,servernode,pids,osocketpind})do
-        #IO.puts "Activating clients, and starting time measurement."
-        #rangemax = :math.pow(10,String.length(Integer.to_string(numClients)-2))
-        #IO.puts subPercent
-        
+        IO.puts "Activating clients."
         n_list = Enum.to_list 1..numClients
         sub_list = Enum.map(1..numClients, fn(_)-> Enum.map(Range.new(1,subPercent), fn(_)-> bais(numClients) end) end)
         Enum.map(n_list, fn(x)-> send Enum.at(pids,x-1), {:activate,Enum.uniq(Enum.at(sub_list,x-1))} end)
-        #start_time = System.system_time(:millisecond)
         {:noreply,{numClients,acts,subPercent,numRegistered,numCompleted,servernode,pids,osocketpind}}
     end
     
     def handle_cast({:acts_completed},{numClients,acts,subPercent,numRegistered,numCompleted,servernode,pids,osocketpind}) do
         numCompleted= numCompleted + 1
         if(numCompleted == numClients) do
-            #Process.sleep(1000)
             IO.puts ("Request generation completed, messages getting delivered. Pls wait.")
             #GenServer.cast({:server,servernode},{:all_completed})
             #send List.last(pids),{:time_to_stop, osocketpind}
@@ -73,12 +68,3 @@ defmodule Orc do
         end
     end
 end
-
-# def handle_cast({:simulate_disconnection},{numClients,acts,subPercent,numRegistered,numCompleted,servernode,start_time}) do
-#     client = :rand.uniform(numClients)
-#     time = :rand.uniform(5)*500
-#     GenServer.cast(String.to_atom("user"<>Integer.to_string(client)),{:disconnect,time})
-#     Process.sleep(3000)
-#     GenServer.cast(self(),{:simulate_disconnection})
-#     {:noreply,{numClients,acts,subPercent,numRegistered,numCompleted,servernode,start_time}}
-# end
